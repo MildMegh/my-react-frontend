9799678eeed5e11d9ac5dc429a94c11ecedbbdc2
Megh Shrestha <meghg2021@gmail.com>
2025-08-21 15:13:37 -0400

    Added register and signup UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Fetching from './Connectionends/Fetchingdata';
+import Register from './Components/RegisterForm';
+import SignIn from './Components/SignIn';
+import UsersRegistered from './Components/UsersRegistered';
 
 function App() {
   return (
-    
+    <>
     <Fetching/>
+    <Register/>
+    <SignIn/>
+    <UsersRegistered/>
+    </>
   );
 }
 
